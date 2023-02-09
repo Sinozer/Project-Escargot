2d305778b745f3ba1443fab71ffc58e9d1eda1d2
Théo <tdasilva@gaming.tech>
2023-02-09 20:56:18 +0100

    :lock: Adding a clamp surcharge to use it with a vector2f

--- Project Escargot/Game/Math/Math.h
+++ Project Escargot/Game/Math/Math.h
@@ -12,5 +12,12 @@ namespace Snail
             
             return std::min(std::max(value, min), max);
 		}
+		static sf::Vector2f inline Clamp(sf::Vector2f value, float min, float max)
+		{
+			if (min > max)
+				throw std::invalid_argument("Min is greater than max");
+
+			return sf::Vector2f(std::min(std::max(value.x, min), max), std::min(std::max(value.y, min), max));
+		}
 	};
 }
