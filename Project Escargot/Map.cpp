d78b10b74ff8f346806339aa5dc77a13d259090e
Théo <tdasilva@gaming.tech>
2023-02-10 00:03:44 +0100

    :lock: Added a new Map class

---
+++ Project Escargot/Map.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "Map.h"
+
+namespace Snail
+{
+	Map::Map()
+	{
+	}
+
+	Map::~Map()
+	{
+	}
+
+	void Map::Init()
+	{
+	}
+
+	void Map::Update()
+	{
+	}
+
+	void Map::Draw()
+	{
+	}
+}
