a152d300c4537b102a5e6ea12fffe62d86e7b3f3
Théo <tdasilva@gaming.tech>
2023-02-09 23:44:18 +0100

    :anchor: Removing useless "this->"

--- Project Escargot/Game/Asset/AssetManager.cpp
+++ Project Escargot/Game/Asset/AssetManager.cpp
@@ -6,23 +6,23 @@ namespace Snail
 	{
 		sf::Texture texture;
 		if (texture.loadFromFile(fileName))
-			this->m_textures[name] = texture;
+			m_textures[name] = texture;
 	}
 
 	sf::Texture& AssetManager::GetTexture(std::string name)
 	{
-		return this->m_textures.at(name);
+		return m_textures.at(name);
 	}
 
 	void AssetManager::LoadFont(std::string name, std::string fileName)
 	{
 		sf::Font font;
 		if (font.loadFromFile(fileName))
-			this->m_fonts[name] = font;
+			m_fonts[name] = font;
 	}
 
 	sf::Font& AssetManager::GetFont(std::string name)
 	{
-		return this->m_fonts.at(name);
+		return m_fonts.at(name);
 	}
 }
