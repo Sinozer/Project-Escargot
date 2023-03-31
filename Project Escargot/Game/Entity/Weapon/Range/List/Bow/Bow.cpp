2436fb2e7472a2be69270c223e239bd4e4a04f47
Théo <tdasilva@gaming.tech>
2023-03-31 11:35:14 +0200

    :anchor: Modified the bow physic body which is used as a pointer to not have collisions

--- Project Escargot/Game/Entity/Weapon/Range/List/Bow/Bow.cpp
+++ Project Escargot/Game/Entity/Weapon/Range/List/Bow/Bow.cpp
@@ -25,7 +25,7 @@ namespace Snail
 	void Bow::m_InitPhysicBody(PhysicBodyManager &pbm)
 	{
 		m_physicBodyRef = PhysicBodyRef(PhysicBody::CreateBoxBody(
-			sf::Vector2f(5.f, 5.f), sf::Vector2f(150, 150), 0.f, false
+			sf::Vector2f(5.f, 5.f), sf::Vector2f(150, 150), 0.f, true, false, false
 		));
 
 		pbm.AddPhysicBody("BOW", m_physicBodyRef);
